e6b1e085f6c2d820e8b4b252c94a563673990ba4
qingshan754 <2041105585@qq.com>
2025-04-04 23:44:05 +0800

    opencv打开电脑上的图像

---
+++ opencv_day1.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#调用文件夹中的照片
+img=cv2.imread('E:\证件照\mmexport1695807557875.jpg')
+#显示照片
+cv2.imshow('image',img)
+#等待时间，0表示毫秒
+cv2.waitKey(0)
+#关闭窗口
+cv2.destroyAllWindows()
+
+
