d271d9274340589e74ea770b8c5435c224be3a8d
qingshan754 <2041105585@qq.com>
2025-04-05 22:37:14 +0800

    Add files via upload

--- opencv_day1.py
+++ opencv_day1.py
@@ -2,12 +2,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 #调用文件夹中的照片
-img=cv2.imread('E:\证件照\mmexport1695807557875.jpg')
+img=cv2.imread(r'E:\证件照\mmexport1695807557875.jpg')
 #显示照片
 cv2.imshow('image',img)
 #等待时间，0表示毫秒
 cv2.waitKey(0)
 #关闭窗口
 cv2.destroyAllWindows()
+
+print(img.size)
 
 
